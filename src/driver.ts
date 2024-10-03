ce5295966569dfd64fda182ecf6a1ebafe164030
tomlap <tomlaplace0902@gmail.com>
2024-10-03 16:57:38 +0200

    feature : blizzard oauth driver

--- src/driver.ts
+++ src/driver.ts
@@ -14,33 +14,22 @@
 |--------------------------------------------------------------------------
  */
 
-import { Oauth2Driver } from '@adonisjs/ally'
+import { Oauth2Driver, RedirectRequest } from '@adonisjs/ally'
 import type { HttpContext } from '@adonisjs/core/http'
-import type { AllyDriverContract, AllyUserContract, ApiRequestContract } from '@adonisjs/ally/types'
+import type {
+  AllyDriverContract,
+  AllyUserContract,
+  ApiRequestContract,
+  LiteralStringUnion,
+} from '@adonisjs/ally/types'
 
-/**
- *
- * Access token returned by your driver implementation. An access
- * token must have "token" and "type" properties and you may
- * define additional properties (if needed)
- */
 export type BlizzardDriverAccessToken = {
   token: string
   type: 'bearer'
 }
 
-/**
- * Scopes accepted by the driver implementation.
- */
-export type BlizzardDriverScopes =
-  | 'wow.profile'
-  | 'sc2.profile'
-  | 'd3.profile'
-  | 'openid'
+export type BlizzardDriverScopes = 'wow.profile' | 'sc2.profile' | 'd3.profile' | 'openid'
 
-/**
- * The configuration accepted by the driver implementation.
- */
 export type BlizzardDriverConfig = {
   clientId: string
   clientSecret: string
@@ -48,74 +37,21 @@ export type BlizzardDriverConfig = {
   authorizeUrl?: string
   accessTokenUrl?: string
   userInfoUrl?: string
+  scopes?: LiteralStringUnion<BlizzardDriverScopes>[]
 }
 
-/**
- * Driver implementation. It is mostly configuration driven except the API call
- * to get user info.
- */
 export class BlizzardDriver
   extends Oauth2Driver<BlizzardDriverAccessToken, BlizzardDriverScopes>
-  implements AllyDriverContract<BlizzardDriverAccessToken, BlizzardDriverScopes> {
-  /**
-   * The URL for the redirect request. The user will be redirected on this page
-   * to authorize the request.
-   *
-   * Do not define query strings in this URL.
-   */
+  implements AllyDriverContract<BlizzardDriverAccessToken, BlizzardDriverScopes>
+{
   protected authorizeUrl = 'https://oauth.battle.net/authorize'
-
-  /**
-   * The URL to hit to exchange the authorization code for the access token
-   *
-   * Do not define query strings in this URL.
-   */
   protected accessTokenUrl = 'https://oauth.battle.net/token'
-
-  /**
-   * The URL to hit to get the user details
-   *
-   * Do not define query strings in this URL.
-   */
   protected userInfoUrl = 'https://oauth.battle.net/oauth/userinfo'
-
-  /**
-   * The param name for the authorization code. Read the documentation of your oauth
-   * provider and update the param name to match the query string field name in
-   * which the oauth provider sends the authorization_code post redirect.
-   */
   protected codeParamName = 'code'
-
-  /**
-   * The param name for the error. Read the documentation of your oauth provider and update
-   * the param name to match the query string field name in which the oauth provider sends
-   * the error post redirect
-   */
   protected errorParamName = 'error'
-
-  /**
-   * Cookie name for storing the CSRF token. Make sure it is always unique. So a better
-   * approach is to prefix the oauth provider name to `oauth_state` value. For example:
-   * For example: "facebook_oauth_state"
-   */
   protected stateCookieName = 'blizzard_oauth_state'
-
-  /**
-   * Parameter name to be used for sending and receiving the state from.
-   * Read the documentation of your oauth provider and update the param
-   * name to match the query string used by the provider for exchanging
-   * the state.
-   */
   protected stateParamName = 'state'
-
-  /**
-   * Parameter name for sending the scopes to the oauth provider.
-   */
   protected scopeParamName = 'scope'
-
-  /**
-   * The separator indentifier for defining multiple scopes
-   */
   protected scopesSeparator = ' '
 
   constructor(
@@ -124,80 +60,72 @@ export class BlizzardDriver
   ) {
     super(ctx, config)
 
-    /**
-     * Extremely important to call the following method to clear the
-     * state set by the redirect request.
-     *
-     * DO NOT REMOVE THE FOLLOWING LINE
-     */
     this.loadState()
   }
 
-  /**
-   * Optionally configure the authorization redirect request. The actual request
-   * is made by the base implementation of "Oauth2" driver and this is a
-   * hook to pre-configure the request.
-   */
+  protected configureRedirectRequest(request: RedirectRequest<BlizzardDriverScopes>){
+    request.scopes(this.config.scopes || ['wow.profile'])
+
+    request.param('response_type', 'code')
+    request.param('grant_type', 'authorization_code')
+  }
+
   // protected configureRedirectRequest(request: RedirectRequest<BlizzardDriverScopes>) {}
 
-  /**
-   * Optionally configure the access token request. The actual request is made by
-   * the base implementation of "Oauth2" driver and this is a hook to pre-configure
-   * the request
-   */
   // protected configureAccessTokenRequest(request: ApiRequest) {}
 
-  /**
-   * Update the implementation to tell if the error received during redirect
-   * means "ACCESS DENIED".
-   */
   accessDenied() {
     return this.ctx.request.input('error') === 'user_denied'
   }
 
-  /**
-   * Get the user details by query the provider API. This method must return
-   * the access token and the user details both. Checkout the google
-   * implementation for same.
-   *
-   * https://github.com/adonisjs/ally/blob/develop/src/Drivers/Google/index.ts#L191-L199
-   */
   async user(
     callback?: (request: ApiRequestContract) => void
   ): Promise<AllyUserContract<BlizzardDriverAccessToken>> {
-    const accessToken = await this.accessToken()
-    const request = this.httpClient(this.config.userInfoUrl || this.userInfoUrl)
+    const accessToken = await this.accessToken(callback)
+    const user = await this.getUserInfo(accessToken.token, callback)
 
-    /**
-     * Allow end user to configure the request. This should be called after your custom
-     * configuration, so that the user can override them (if needed)
-     */
-    if (typeof callback === 'function') {
-      callback(request)
+    return {
+      ...user,
+      token: accessToken,
     }
-
-    /**
-     * Write your implementation details here.
-     */
   }
 
   async userFromToken(
     accessToken: string,
     callback?: (request: ApiRequestContract) => void
   ): Promise<AllyUserContract<{ token: string; type: 'bearer' }>> {
+    const user = await this.getUserInfo(accessToken, callback)
+
+    return {
+      ...user,
+      token: {
+        token: accessToken,
+        type: 'bearer',
+      },
+    }
+  }
+
+  protected getAuthenticatedRequest(token: string) {
     const request = this.httpClient(this.config.userInfoUrl || this.userInfoUrl)
 
-    /**
-     * Allow end user to configure the request. This should be called after your custom
-     * configuration, so that the user can override them (if needed)
-     */
+    request.header('Authorization', `Bearer ${token}`)
+    request.header('Accept', 'application/json')
+    request.param('format', 'json')
+    request.parseAs('json')
+
+    return request
+  }
+
+  protected async getUserInfo(token: string, callback?: (request: ApiRequestContract) => void) {
+    const request = this.getAuthenticatedRequest(token)
+
     if (typeof callback === 'function') {
       callback(request)
     }
 
-    /**
-     * Write your implementation details here
-     */
+    const body = await request.get()
+
+    return body
   }
 }
 
@@ -205,6 +133,8 @@ export class BlizzardDriver
  * The factory function to reference the driver implementation
  * inside the "config/ally.ts" file.
  */
-export function BlizzardDriverService(config: BlizzardDriverConfig): (ctx: HttpContext) => BlizzardDriver {
+export function BlizzardDriverService(
+  config: BlizzardDriverConfig
+): (ctx: HttpContext) => BlizzardDriver {
   return (ctx) => new BlizzardDriver(ctx, config)
 }
